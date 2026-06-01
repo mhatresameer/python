3f935b2b165c51b514dba70fe4f00221c0bbfabd
M. Sameer <mhatresameer@outlook.com>
2026-06-02 01:15:47 +0530

    Create 1.4.10_variable_string_plus_integer.py

---
+++ 1.4.10_variable_string_plus_integer.py
@@ -0,0 +1,11 @@
+# Level 3: Common Interview Questions
+""" Exercise 10: String + Integer
+Fix the code:
+name = "Sameer"
+age = 28
+print("My age is " + age) """
+
+name = "Sameer"
+age = 28
+print("My age is ", age)
+# can only concatenate str (not "int") to str.
