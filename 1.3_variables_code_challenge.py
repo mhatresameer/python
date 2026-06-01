822c88bcf39116a55c713a312a48bd450722c3d1
M. Sameer <mhatresameer@outlook.com>
2026-06-01 17:13:58 +0530

    Create 1.3_variables_code_challenge.py

---
+++ 1.3_variables_code_challenge.py
@@ -0,0 +1,8 @@
+# Create a variable x and assign it the value 5
+# Create a variable y and assign it the value "John"
+# Use the type() function to print the type of x
+
+x = 5
+y = "John"
+
+print(type(x))
