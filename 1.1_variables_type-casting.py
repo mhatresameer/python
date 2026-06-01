c067f0c2865735f5856c5f0ed055eb84e19b2914
M. Sameer <mhatresameer@outlook.com>
2026-06-01 16:40:45 +0530

    Create 1.1_variables_type-casting.py

---
+++ 1.1_variables_type-casting.py
@@ -0,0 +1,12 @@
+# if we want to specify the data-types and change it accordingly, we use casting for variables.
+username = "Sameer"
+type(username)
+
+# we are assigning a variable the number which is a whole number and an integer.
+age = 30
+print(age)
+# knowing the type before we type-cast.
+type(age)
+
+# here we just type-cast the existing integer to float by printing it and assigning it directly.
+print(float(age))
