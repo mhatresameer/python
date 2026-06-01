f6cb9fa0a6449a51ae06019164ba5c3a8cbe2464
M. Sameer <mhatresameer@outlook.com>
2026-06-02 02:15:41 +0530

    Update 1.4.0_variables_excercises_and_problem-sets.py

--- 1.4.0_variables_excercises_and_problem-sets.py
+++ 1.4.0_variables_excercises_and_problem-sets.py
@@ -8,3 +8,16 @@
 """ Exercise 5: User Information. Take user's input. (1.4.5) """
 """ Exercise 6: Rectangle Area. Take: Length, Width, Store them in variables and calculate area. Formula: area = length * width (1.4.6) """
 """ Exercise 7: Temperature Converter. Convert Celsius to Fahrenheit. Formula: F = (C * 9/5) + 32 (1.4.7) """
+
+# Level 3: Common Interview Questions
+""" Exercise 8: Multiple Assignment. Predict output: a, b, c = 1, 2, 3 (1.4.8) """
+""" Exercise 9: Variable Memory Concept. What happens here? (1.4.9) """
+""" Exercise 10: String + Integer (1.4.10) """
+""" Exercise 11: Salary Increment. Take salary as input and increase it by 10%. (1.4.11) """
+
+# Level 4: Interview Coding Problems
+""" Excercise 12: Variables Exchange Money (1.4.12) """
+""" Excercise 13: Reverse Variable Values (1.4.13) """
+""" Excercise 14: Variables Seconds (Time) Conversion (1.4.14) """
+""" Excercise 12: Variables Simple Interest (1.4.15) """
+
