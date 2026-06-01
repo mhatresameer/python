1ac23b6124015e2e245324581ca8ca9a5b212495
M. Sameer <mhatresameer@outlook.com>
2026-06-01 22:28:59 +0530

    Update 1.4_variables_excercises_and_problem-sets.py

--- 1.4_variables_excercises_and_problem-sets.py
+++ 1.4_variables_excercises_and_problem-sets.py
@@ -72,3 +72,12 @@ length = int(input("Enter the length: "))
 width = int(input("Enter the width: "))
 area = length * width
 print(float(area))
+
+# Exercise 7: Temperature Converter
+""" Convert Celsius to Fahrenheit.
+Formula: F = (C * 9/5) + 32
+Input: 25
+Output: 77.0 """
+celsius = float(input("Enter the temperature in celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"The Fahrenheit is {fahrenheit}")
