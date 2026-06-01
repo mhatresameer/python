8c7c46e0a8e850c3d29f4a52f0c97a564e9ca39b
M. Sameer <mhatresameer@outlook.com>
2026-06-02 01:56:46 +0530

    Create 1.4.13_reverse_variable_values.py

---
+++ 1.4.13_reverse_variable_values.py
@@ -0,0 +1,7 @@
+
+""" Exercise 13: Reverse Variable Values. Using variables only (no loops), reverse it. """
+
+user_reverse_number = int(input("Enter the number you wish to reverse: "))
+# take a variable to store the reverse string
+original_reverse = int(str(user_reverse_number)[::-1])
+print(original_reverse)
