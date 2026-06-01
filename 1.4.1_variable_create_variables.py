cd4f1d7ddc5fab6196bbbcdba5c9652c260e6593
M. Sameer <mhatresameer@outlook.com>
2026-06-01 22:41:43 +0530

    Create 1.4.1_variable_create_variables.py

---
+++ 1.4.1_variable_create_variables.py
@@ -0,0 +1,15 @@
+# Level 1: Beginner (Understanding Variables)
+
+""" Exercise 1: Create Variables
+Create variables to store:
+Your name
+Age
+City
+Current job title
+Print them in a single sentence. """
+your_name = "Sameer"
+your_age = 30
+your_city = "Mumbai"
+your_current_job_title = "Senior Engineer"
+# expected o/p: My name is Sameer, I am 28 years old and I live in Panvel.
+print(f"My name is {your_name}, I am {your_age} years old and I live in {your_city}")
