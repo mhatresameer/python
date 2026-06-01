a0b5547ce76ff8e901bb0aab5c268c8ec0b62d06
M. Sameer <mhatresameer@outlook.com>
2026-06-02 01:38:02 +0530

    Create 1.4.11_salary_increment.py

---
+++ 1.4.11_salary_increment.py
@@ -0,0 +1,8 @@
+# Level 3: Common Interview Questions
+""" Exercise 11: Salary Increment. Take salary as input and increase it by 10%. """
+
+user_salary = int(input("Enter your salary: "))
+new_user_salary = user_salary * 0.10
+final_salary = user_salary + new_user_salary
+
+print(final_salary)
