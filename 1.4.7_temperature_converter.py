adbb0e300933fb722c53e2e7d0c5cb4bd28cca2e
M. Sameer <mhatresameer@outlook.com>
2026-06-01 23:06:32 +0530

    Create 1.4.7_temperature_converter.py

---
+++ 1.4.7_temperature_converter.py
@@ -0,0 +1,10 @@
+# Level 2: Input + Variables
+# Exercise 7: Temperature Converter
+""" Convert Celsius to Fahrenheit.
+Formula: F = (C * 9/5) + 32
+Input: 25
+Output: 77.0 """
+
+celsius = float(input("Enter the temperature in celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"The Fahrenheit is {fahrenheit}")
