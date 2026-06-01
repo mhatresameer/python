0128208f8b6d2d089db6c2a23d80062a04b7729a
M. Sameer <mhatresameer@outlook.com>
2026-06-01 16:29:10 +0530

    Update variables_and_data-types.py

--- variables_and_data-types.py
+++ variables_and_data-types.py
@@ -1,1 +1,12 @@
+# variables are like containers that store values irrespective of their data types.
+username = "Sameer"
+age = 30
+annual_salary = 6.5
+is_working = True
+has_completed_masters = False
 
+print(username)
+print(age)
+print(annual_salary)
+print(is_working)
+print(has_completed_masters)
