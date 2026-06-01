e57646be76f96fc36c55e4cc23f80be78c02c50b
M. Sameer <mhatresameer@outlook.com>
2026-06-01 23:05:26 +0530

    Create 1.4.5_user_information.py

---
+++ 1.4.5_user_information.py
@@ -0,0 +1,13 @@
+# Level 2: Input + Variables
+""" Exercise 5: User Information
+Take user's:
+Name
+Age
+Print:
+Hello Sameer, next year you will be 29 years old. """
+
+username_input = input("Enter your name here: ")
+username_age = int(input("Enter your name here: "))
+username_age += 1
+
+print(f"Hello {username_input}, next year you will be {username_age} years old.")
