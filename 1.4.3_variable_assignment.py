4e153a858c667f0348be125c1b07867d31a67be0
M. Sameer <mhatresameer@outlook.com>
2026-06-01 23:03:52 +0530

    Create 1.4.3_variable_assignment.py

---
+++ 1.4.3_variable_assignment.py
@@ -0,0 +1,10 @@
+# Level 1: Beginner (Understanding Variables)
+# Exercise 3: Variable Reassignment
+# What will be the final value of x?
+x = 5
+x = x + 3
+x = x * 2
+x = x - 4
+print(x)
+
+# answer is 12
