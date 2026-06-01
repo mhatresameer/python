a7d2588f51b783ebc8a6bd12d067dca24c85b408
M. Sameer <mhatresameer@outlook.com>
2026-06-01 23:04:31 +0530

    Create 1.4.4_swap_two_variables.py

---
+++ 1.4.4_swap_two_variables.py
@@ -0,0 +1,15 @@
+# Level 1: Beginner (Understanding Variables)
+# Exercise 4: Swap Two Variables
+""" Swap the values without using a third variable.
+a = 10
+b = 20
+Expected:
+a = 20
+b = 10 """
+
+a = 10
+b = 20
+# use swap method
+a, b = b, a
+print(a)
+print(b)
