ac73c2076e432f12e41c74b9a95de59336004542
M. Sameer <mhatresameer@outlook.com>
2026-06-01 23:03:13 +0530

    Create 1.4.2_find_the_data-type.py

---
+++ 1.4.2_find_the_data-type.py
@@ -0,0 +1,11 @@
+# Level 1: Beginner (Understanding Variables)
+# Exercise 2: Find the Data Type
+# Without running the code, predict the output.
+a = 10
+b = 10.5
+c = "10"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
