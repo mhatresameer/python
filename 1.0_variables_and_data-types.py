31bd22152d827570a4fd344c37b4591f757ac24c
M. Sameer <mhatresameer@outlook.com>
2026-06-01 16:49:51 +0530

    Update 1.0_variables_and_data-types.py

--- 1.0_variables_and_data-types.py
+++ 1.0_variables_and_data-types.py
@@ -12,3 +12,23 @@ print(is_working)
 print(has_completed_masters)
 
 # a variables value can be changes at the moment of assigning it. Which means it doesnt hold any types.
+
+# case-sensetive - Python is case sensetive and below both scenarios are different variable names.
+username = "username is one variable in case-sensetive."
+Username = "Username is a different variable in case-sensetive."
+
+print(Username)
+
+# declare a variable name in Python
+
+# CAMEL CASE
+userName = "thisIsCamelCaseInPython."
+print(userName)
+
+# PASCAL CASE - first letter is uppercase
+UserName = "PASCAL CASE in VARIABLES."
+print(UserName)
+
+# SNAKE CASE - we have underscore after each word
+user_name_here = "This_is_a_snake_case_in_variables."
+print(user_name_here)
