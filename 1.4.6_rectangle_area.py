63230624dabf4ace800932cccba4a48bf9b912d3
M. Sameer <mhatresameer@outlook.com>
2026-06-01 23:05:57 +0530

    Create 1.4.6_rectangle_area.py

---
+++ 1.4.6_rectangle_area.py
@@ -0,0 +1,13 @@
+# Level 2: Input + Variables
+# Exercise 6: Rectangle Area
+"""Take:
+Length
+Width
+Store them in variables and calculate area.
+Formula:
+area = length * width"""
+
+length = int(input("Enter the length: "))
+width = int(input("Enter the width: "))
+area = length * width
+print(float(area))
