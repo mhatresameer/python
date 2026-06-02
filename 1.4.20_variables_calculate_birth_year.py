59f53cdf40b814c1f98c22eb4067e5c08e5280e1
M. Sameer <mhatresameer@outlook.com>
2026-06-02 12:16:31 +0530

    Create 1.4.20_variables_calculate_birth_year.py

---
+++ 1.4.20_variables_calculate_birth_year.py
@@ -0,0 +1,6 @@
+# Level 5: Mini Interview Challenge
+""" Problem 5 (Junior Developer Interview Favorite) - Take a person's birth year as input and calculate their age. """
+
+birth_year = int(input("Enter your birth year: "))
+age = 2026 - birth_year
+print(f"Your age as of today in 2026 is {age}")
