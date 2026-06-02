888a60543604870058e757a9f7773ffd22cbabc4
M. Sameer <mhatresameer@outlook.com>
2026-06-03 01:36:01 +0530

    Create 2.1.5_convert_given_num_into_string_and_float_and_print.py

---
+++ 2.1.5_convert_given_num_into_string_and_float_and_print.py
@@ -0,0 +1,14 @@
+# Level 2: Type Conversion
+""" Convert given num (25) into string and float and print all 3 versions. """
+
+num = 25
+num1 = str(num)
+num2 = float(num)
+
+print(num)
+print(num1)
+print(num2)
+
+print(type(num))
+print(type(num1))
+print(type(num2))
