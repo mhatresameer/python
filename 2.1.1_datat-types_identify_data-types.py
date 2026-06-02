6cbe063a2825d04085576f4017d7f9721ebd23cb
M. Sameer <mhatresameer@outlook.com>
2026-06-03 00:42:08 +0530

    Create 2.1.1_datat-types_identify_data-types.py

---
+++ 2.1.1_datat-types_identify_data-types.py
@@ -0,0 +1,12 @@
+# Level 1: Identify the Data Type
+""" Exercise 2: What are the data types of these values? """
+
+100 # int
+100.5 # float
+"Python" # string
+False # boolean
+[1, 2, 3] # list
+(1, 2, 3) # tuple
+{"name": "Sam"} # dictionary / dict
+{1, 2, 3} # set
+None # noneType
