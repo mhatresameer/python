cd163141d06c8d752b792cf23bdb22b33be2f5db
M. Sameer <mhatresameer@outlook.com>
2026-06-03 01:22:03 +0530

    Update 2.1.0_data-types_excercises_and_problem-sets.py

--- 2.1.0_data-types_excercises_and_problem-sets.py
+++ 2.1.0_data-types_excercises_and_problem-sets.py
@@ -1,2 +1,3 @@
 # Level 1: Identify the Data Type
 """ Problem 1: Identify the Data Type (2.1.1) """
+""" Problem 2: Which of the following are mutable? - list, tuple, dictionary, string, set. Explain why. (2.1.2) """
