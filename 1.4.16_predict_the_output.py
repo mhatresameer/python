d4850a04578030ba4d35c34ba34e13548e1f0d7e
M. Sameer <mhatresameer@outlook.com>
2026-06-02 11:12:06 +0530

    Create 1.4.16_predict_the_output.py

---
+++ 1.4.16_predict_the_output.py
@@ -0,0 +1,17 @@
+# Level 5: Mini Interview Challenge
+""" Problem 1 - Predict the output """
+
+a = 5
+b = a
+a = a + 5
+print(a)
+print(b)
+
+"""
+a = 5
+b = 5
+a = 5+5
+"""
+
+print(a) #10
+print(b) #5
