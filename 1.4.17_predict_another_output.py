53df15529d8f3bafb82bea2aae670920a50ada8e
M. Sameer <mhatresameer@outlook.com>
2026-06-02 11:14:12 +0530

    Create 1.4.17_predict_another_output.py

---
+++ 1.4.17_predict_another_output.py
@@ -0,0 +1,21 @@
+# Level 5: Mini Interview Challenge
+""" Problem 2 - Predict: """
+
+x = 10
+y = 20
+x, y = y, x
+x = x + y
+
+print(x)
+print(y)
+
+"""
+x = 10
+y = 20
+x = 20
+y = 10
+
+x = 20 + 10
+30
+10
+"""
