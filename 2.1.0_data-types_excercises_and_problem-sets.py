ac5ef2845a65d4bee4af46d4eef1f06cc0a26261
M. Sameer <mhatresameer@outlook.com>
2026-06-03 01:36:11 +0530

    Update 2.1.0_data-types_excercises_and_problem-sets.py

--- 2.1.0_data-types_excercises_and_problem-sets.py
+++ 2.1.0_data-types_excercises_and_problem-sets.py
@@ -1,3 +1,8 @@
 # Level 1: Identify the Data Type
 """ Problem 1: Identify the Data Type (2.1.1) """
 """ Problem 2: Which of the following are mutable? - list, tuple, dictionary, string, set. Explain why. (2.1.2) """
+
+# Level 2: Type Conversion
+""" Problem 3: Convert given number into INT and add 5. (2.1.3) """
+""" Problem 4: Fix the code (2.1.4) """
+""" Problem 5: Convert an integer into string and float and print all 3 versions. (2.1.5) """
