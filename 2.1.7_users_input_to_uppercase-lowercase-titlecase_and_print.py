52843897ced767c2613cf91e6db07e266eb9ef84
M. Sameer <mhatresameer@outlook.com>
2026-06-03 14:06:08 +0530

    Create 2.1.7_users_input_to_uppercase-lowercase-titlecase_and_print.py

---
+++ 2.1.7_users_input_to_uppercase-lowercase-titlecase_and_print.py
@@ -0,0 +1,11 @@
+# Level 3: String Data Type
+""" Take users input and print it in uppercase, lowercase and titlecase """
+
+user_name = input("Enter your username to check it in uppercase, lowercase and titlecase: ")
+uppercase_username = user_name.upper()
+lowercase_username = user_name.lower()
+titlecase_username = user_name.title()
+
+print(uppercase_username)
+print(lowercase_username)
+print(titlecase_username)
