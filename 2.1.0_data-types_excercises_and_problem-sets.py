b5e4d9e521f0d3d46329e148b15e49e77fde2f5f
M. Sameer <mhatresameer@outlook.com>
2026-06-03 14:34:41 +0530

    Update 2.1.0_data-types_excercises_and_problem-sets.py

--- 2.1.0_data-types_excercises_and_problem-sets.py
+++ 2.1.0_data-types_excercises_and_problem-sets.py
@@ -6,3 +6,12 @@
 """ Problem 3: Convert given number into INT and add 5. (2.1.3) """
 """ Problem 4: Fix the code (2.1.4) """
 """ Problem 5: Convert an integer into string and float and print all 3 versions. (2.1.5) """
+
+# Level 3: String Data Type
+""" Find the length of: language = "Programming". Without counting manually (2.1.6) """
+""" Take users input and print it in uppercase, lowercase and titlecase (2.1.7) """
+
+# Level 4: Lists
+""" Add a new value to the lists (2.1.8) """
+""" Remove a value from the lists (2.1.9) """
+""" Find first_element, last_element and length of the list provided (2.1.10) """
