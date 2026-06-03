27806017a02dd302dc586ab8694de6dac4777a2f
M. Sameer <mhatresameer@outlook.com>
2026-06-03 14:34:36 +0530

    Create 2.1.10_find_length-first_element-last_element.py

---
+++ 2.1.10_find_length-first_element-last_element.py
@@ -0,0 +1,11 @@
+# Level 4: Lists
+"""
+Exercise 14
+Find: Length, First element, Last element of: languages = ["Python", "Java", "C++", "Go"]
+"""
+
+languages = ["Python", "Java", "C++", "Go"]
+print(languages)
+
+print(f"The length of the list is: {len(languages)}")
+print(f"The first element of the list is '{languages[0]}', and the last element is '{languages[3]}'")
