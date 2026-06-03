34a7f812a495ee6501dff8455ec4cf5a1029411b
M. Sameer <mhatresameer@outlook.com>
2026-06-03 14:56:56 +0530

    Create 2.1.14_dict_add_new_key_in_dictionary.py

---
+++ 2.1.14_dict_add_new_key_in_dictionary.py
@@ -0,0 +1,18 @@
+# Level 6: Dictionaries
+"""
+Exercise 19
+Add a new key:
+"job": "Engineer"
+"""
+
+# taking dictionary from previous example:
+student = {
+   "name": "Sameer",
+   "age": 28,
+   "city": "Mumbai"
+}
+
+student["job"] = "Engineer"
+print(student)
+
+# O/P: {'name': 'Sameer', 'age': 28, 'city': 'Mumbai', 'job': 'Engineer'}
