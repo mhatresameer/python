01b92e2fd416a35fa220142b0fadd3bbf7cc84a1
M. Sameer <mhatresameer@outlook.com>
2026-06-04 00:34:24 +0530

    Create 2.1.23_junior_developer_interview_questions_type_checker.py

---
+++ 2.1.23_junior_developer_interview_questions_type_checker.py
@@ -0,0 +1,27 @@
+# Level 9: Junior Developer Interview Challenges
+
+"""
+Take input from the user.
+Print:
+Original Value
+Original Type
+Converted Integer
+Converted Float
+Handle valid numeric inputs.
+"""
+
+user_number = input("Enter the number: ")
+print(user_number)
+print("\n")
+"""
+Print:
+"""
+# Original Value
+print(f"The original value entered is: {user_number}")
+# Original Type
+print(f"The original type entered is: {type(user_number)}")
+# Converted Integer
+print(f"The original value entered is: {int(user_number)}")
+# Converted Float
+print(f"The original value entered is: {float(user_number)}")
+# Handle valid numeric inputs.
