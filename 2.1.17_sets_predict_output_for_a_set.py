84006f8feeba1b1258946549f667c14d99e7b882
M. Sameer <mhatresameer@outlook.com>
2026-06-03 15:13:47 +0530

    Create 2.1.17_sets_predict_output_for_a_set.py

---
+++ 2.1.17_sets_predict_output_for_a_set.py
@@ -0,0 +1,12 @@
+# Level 7: Sets
+"""
+Predict output:
+nums = {1, 2, 2, 3, 3, 4}
+print(nums)
+"""
+
+nums = {1, 2, 2, 3, 3, 4}
+print(nums)
+# O/P: {1, 2, 3, 4}
+# When you create a set with duplicate values, Python automatically removes the duplicates, so the set will only contain 1, 2, 3, 4.
+# The print(nums) statement then displays this set to the console.
