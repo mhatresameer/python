1fc627c1150efe1d1c9ba289dee914a75549118c
M. Sameer <mhatresameer@outlook.com>
2026-06-03 14:39:10 +0530

    Create 2.1.11_tuple_what_will_happen.py

---
+++ 2.1.11_tuple_what_will_happen.py
@@ -0,0 +1,12 @@
+# Level 5: Tuples
+"""
+Exercise 16: What happens?
+point = (10, 20)
+point[0] = 50
+# Why?
+"""
+
+# either throws an error or the value wouldnt get appended since TUPLE is immutable.
+"""
+throws error: 'tuple' object does not support item assignment.
+"""
