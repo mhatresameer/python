3187eec0ef8741dc6fec7f554f22171df8bd87c0
M. Sameer <mhatresameer@outlook.com>
2026-06-03 14:12:53 +0530

    Create 2.1.8_lists_add_new_value_to_list.py

---
+++ 2.1.8_lists_add_new_value_to_list.py
@@ -0,0 +1,14 @@
+# Level 4: Lists
+"""
+Exercise 12: Add a new value to:
+fruits = ["apple", "banana"]
+Add "orange".
+Expected:
+['apple', 'banana', 'orange']
+"""
+
+fruits = ["Apple", "Banana"]
+print(fruits)
+
+fruits.append("Orange")
+print(fruits)
