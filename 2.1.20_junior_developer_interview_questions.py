7258477f47d4af51202c8fac50b30103bc17f3a2
M. Sameer <mhatresameer@outlook.com>
2026-06-03 20:10:56 +0530

    Create 2.1.20_junior_developer_interview_questions.py

---
+++ 2.1.20_junior_developer_interview_questions.py
@@ -0,0 +1,25 @@
+# Level 9: Junior Developer Interview Challenges
+"""
+Store:
+Name
+Age
+Marks
+Passed (True/False)
+using the most appropriate data types.
+Print all details.
+"""
+
+student_data = {
+"name" : "Sameer Mhatre",
+"age" : 30,
+"marks" : 4.78,
+"if_passed" : True,
+"if_failed" : False
+}
+
+print(type(student_data))
+# to see if it is actually dictionary
+print("\n")
+
+print(f"Good evening {name}. I hope you are doing well today.\nYou are {age} years old and the total CGPA you received is {marks}.\n"
+    f"The total of your marks is {if_passed}. Congratulations once again.")
