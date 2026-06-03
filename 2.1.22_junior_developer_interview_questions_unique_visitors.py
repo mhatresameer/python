542f0c4bed777845cc0a7144603d1bccc9a2cdf7
M. Sameer <mhatresameer@outlook.com>
2026-06-03 20:24:18 +0530

    Create 2.1.22_junior_developer_interview_questions_unique_visitors.py

---
+++ 2.1.22_junior_developer_interview_questions_unique_visitors.py
@@ -0,0 +1,35 @@
+# Level 9: Junior Developer Interview Challenges
+
+"""
+You have:
+visitors = [
+   "Sam",
+   "John",
+   "Sam",
+   "Alice",
+   "John"
+]
+Find unique visitors using a set.
+"""
+# the given data-type is LIST, we need to make sure the data-type is SET and the SET has a feature to hold only unique values.
+visitors = [
+   "Sam",
+   "John",
+   "Sam",
+   "Alice",
+   "John"
+]
+print(type(visitors))
+print(visitors)
+
+# this is SET.
+visitors = {
+   "Sam",
+   "John",
+   "Sam",
+   "Alice",
+   "John"
+}
+
+print(type(visitors))
+print(visitors)
