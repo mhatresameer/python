8c07633ac80fe3dc406df6a73eb7c649ab9556b7
M. Sameer <mhatresameer@outlook.com>
2026-06-03 14:16:29 +0530

    Create 2.1.9_lists_remove_from_list.py

---
+++ 2.1.9_lists_remove_from_list.py
@@ -0,0 +1,11 @@
+# Level 4: Lists
+"""
+Remove "banana" from:
+fruits = ["apple", "banana", "orange"]
+"""
+
+fruits = ["apple", "banana", "orange"]
+print(fruits)
+
+fruits.remove("banana")
+print(fruits)
