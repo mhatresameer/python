551fc7df9f27ba46dbf9a37575157947596c420a
M. Sameer <mhatresameer@outlook.com>
2026-06-03 15:01:01 +0530

    Create 2.1.15_dict_checking_value_not_present_in_dictionary.py

---
+++ 2.1.15_dict_checking_value_not_present_in_dictionary.py
@@ -0,0 +1,16 @@
+# Level 6: Dictionaries
+"""
+Predict the output:
+student = {
+   "name": "Sam",
+   "age": 28
+}
+print(student.get("salary"))
+"""
+student = {
+   "name": "Sam",
+   "age": 28
+}
+print(student.get("salary"))
+
+# O/P: None, since we do not have a dictionary named salary.
