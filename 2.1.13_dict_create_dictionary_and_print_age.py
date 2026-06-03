832e57a8c9477aa039eb2d71e97440e0c3bcf2bf
M. Sameer <mhatresameer@outlook.com>
2026-06-03 14:50:53 +0530

    Create 2.1.13_dict_create_dictionary_and_print_age.py

---
+++ 2.1.13_dict_create_dictionary_and_print_age.py
@@ -0,0 +1,18 @@
+# Level 6: Dictionaries
+"""
+Exercise 18: Create a dictionary and Print only the age.
+student = {
+   "name": "Sameer",
+   "age": 28,
+   "city": "Mumbai"
+}
+"""
+
+student = {
+   "name": "Sameer",
+   "age": 28,
+   "city": "Mumbai"
+}
+
+print(type(student))
+print(student['age'])
