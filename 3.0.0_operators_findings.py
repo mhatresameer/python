9f5c4977d67ee5d244f4ca7a0bfc5e4628e205db
M. Sameer <mhatresameer@outlook.com>
2026-06-04 17:04:46 +0530

    Create 3.0.0_operators_findings.py

---
+++ 3.0.0_operators_findings.py
@@ -0,0 +1,29 @@
+# Level 1: Arithmetic Operators
+""" Find Quotient and Remainder from user input (3.0.1) """
+""" Predict output for power raised """
+
+# Level 2: Assignment Operators
+
+# Level 3: Comparison Operators
+""" 3.0.3_operators_take_2_numbers_and_print_equal_or_not-equal.py """
+
+# Level 4: Logical Operators
+""" Print with condition provided (3.0.4) """
+
+# Level 5: Membership Operators
+""" Check if any word exists in lists (3.0.5) """
+
+# Level 6: Identity Operators
+""" Compare and print equals-to and is operator (3.0.6) """
+
+# Level 7: Mixed Interview Questions
+
+# Level 8: Real Interview Coding Problems
+""" Check even or odd using operator (3.0.7) """
+""" Check if number is divisible by 5 or not (3.0.8) """
+""" Check the largest number from 2 using comparision operator (3.0.9) """
+""" Check user age to check if user can vote or not (3.0.10) """
+""" Check if leap year or not (3.0.11) """
+
+# Level 9: Junior Developer Interview Challenges
+""" Find the last digit of a number (3.0.12) """
