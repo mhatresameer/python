ad8312ab08d88f01a42947e7be4bb277761f0cc7
M. Sameer <mhatresameer@outlook.com>
2026-06-04 14:42:21 +0530

    Create 3.0.6_operators_compare_and_print_equals-to_and_is_operator.py

---
+++ 3.0.6_operators_compare_and_print_equals-to_and_is_operator.py
@@ -0,0 +1,11 @@
+# Level 6: Identity Operators
+
+"""
+Explain the difference. And compare both, and print what will be the output.
+"""
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(a == b) # true - because it compares the value from both variables. Which is if the value from a is equals to b?
+print(a is b) # false - because if checks if one variable is pointing to the same variable? In this case, no. Since A is pointing to B variable. 
