60c49d1a4b61897461e949015256b2535b373ebb
M. Sameer <mhatresameer@outlook.com>
2026-06-04 15:54:10 +0530

    Create 3.0.7_operators_check_even_or_odd_using_operator.py

---
+++ 3.0.7_operators_check_even_or_odd_using_operator.py
@@ -0,0 +1,11 @@
+# Level 8: Real Interview Coding Problems
+
+"""
+Take a number as input.
+Print: Even or Odd using an operator.
+Hint: %
+"""
+
+check_even_or_odd = int(input("Enter any number to check if it is even or odd: "))
+result = check_even_or_odd % 2 == 0
+print(result)
