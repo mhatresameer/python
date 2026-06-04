279f5969d29635347bb8f3e1a04229f5c585b2a7
M. Sameer <mhatresameer@outlook.com>
2026-06-04 16:03:24 +0530

    Create 3.0.9_operators_check_the_largest_number_from_2_using_comparision_operator.py

---
+++ 3.0.9_operators_check_the_largest_number_from_2_using_comparision_operator.py
@@ -0,0 +1,13 @@
+
+
+"""
+Problem 3: Largest Number
+Take two numbers.
+Print the larger number using a comparison operator.
+"""
+
+check_largest_number_1 = int(input("Enter your first number: "))
+check_largest_number_2 = int(input("Enter your second number: "))
+
+result = check_largest_number_1 > check_largest_number_2
+print(f"The largest number is {result}.")
