c62ee40b3b4eee52054e27f1cc5fc2c686242f95
M. Sameer <mhatresameer@outlook.com>
2026-06-04 12:35:46 +0530

    Create 3.0.2_operator_predict_output_for_power_raised.py

---
+++ 3.0.2_operator_predict_output_for_power_raised.py
@@ -0,0 +1,12 @@
+
+
+"""
+What does ** do? - the ** operator is a power to any operator.
+"""
+
+user_power_property = int(input("Enter any number to check the power: "))
+power_raised = int(input("Enter the power to be used with the given number: "))
+result_power_property = user_power_property ** power_raised
+print(f"The number {user_power_property} with power raised to {power_raised} is {result_power_property}")
+
+# What does ** do? - 2^4 which means the 2 raise to the power of 4
