53f47814855b567f58c2e88deb31813c73e318f5
M. Sameer <mhatresameer@outlook.com>
2026-06-10 12:38:26 +0530

    Create 4.0.20_conditional_statements_character_check.py

---
+++ 4.0.20_conditional_statements_character_check.py
@@ -0,0 +1,18 @@
+
+
+"""
+Character Type - Input one character.
+
+Check:
+Uppercase
+Lowercase
+Digit
+Special Character
+"""
+
+character = input("Enter the character to check whether it is Uppercase, Lowercase, Digit or Special Character: ")
+
+if character == A to Z:
+  print("Uppercase")
+else:
+  print("Hello World")
