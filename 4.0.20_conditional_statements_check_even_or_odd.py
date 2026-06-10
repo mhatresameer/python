216fe25f8e326adc4343a47b0b35936efa6bf34e
M. Sameer <mhatresameer@outlook.com>
2026-06-11 00:32:41 +0530

    Create 4.0.20_conditional_statements_check_even_or_odd.py

---
+++ 4.0.20_conditional_statements_check_even_or_odd.py
@@ -0,0 +1,14 @@
+"""
+Even or Odd
+
+Input a number.
+Print "Even" if divisible by 2.
+Otherwise print "Odd".
+"""
+
+number = int(input("Enter a number to check even or odd: "))
+
+if number % 2 == 0:
+  print("EVEN.")
+else:
+  print("ODD.")
