8a9603fe7d29529d75c42d56864fd5d65bd43af1
M. Sameer <mhatresameer@outlook.com>
2026-06-18 14:31:38 +0530

    Update 4.0.50_conditional_statement_parking_fee_calculator.py

--- 4.0.50_conditional_statement_parking_fee_calculator.py
+++ 4.0.50_conditional_statement_parking_fee_calculator.py
@@ -23,35 +23,41 @@ Start
 └─ Display Total Fee
 End
 ---------------------------------------------------
-additional features:
+features implemented : done
 Different rates for Cars, Bikes, and Trucks
 Weekend parking charges
+GST calculation
+---------------------------------------------------
+additional features:
 Lost ticket penalty
 Night parking discount
 Monthly parking passes
-GST calculation
 VIP parking membership discounts
-
-Multiple vehicles
-GST (18%)
-VIP parking pass
-Lost ticket fine
-Overnight parking surcharge
 Receipt number generation
 """
 
-print("We welcome you to Python Parking System. \n")
+# Parking Charges: ₹80
+# Vehicle Charges: ₹20
+# Weekend Charges: ₹100
+# GST (18%): ₹36
+# -------------------
+# Total Amount: ₹236
 
-print("Bike charges - ₹10, Car charges - ₹20, Truck charges - ₹50.\n.")
+print("We welcome you to Python Parking System. \n")
 print("Parking charges are as follows- ")
-print("We charge ₹20/hour for hours between 0 - 2.")
+print("We charge ₹20/hour for hours between 1 - 2.")
 print("We charge ₹15/hour for hours between 3 - 5.")
 print("We charge ₹10/hour for hours between 6 - 10.")
 print("We charge ₹10/hour for hours above 10 with surcharge of ₹150.\n")
+print("Bike charges - ₹10.")
+print("Car charges - ₹20.")
+print("Truck charges - ₹50.\n")
+print("GST = 18% \n")
 
 vehicle_number = input("Enter your vehicle number: ")
 hours_parked = int(input("Enter number of hours parked: "))
 vehicle_type = input("Bike, Car or a Truck?: ").upper()
+parking_on_weekends = input("Weekend Parking? (Yes/NO): ").upper()
 
 if vehicle_type in ["BIKE", "CAR", "TRUCK"]:
     print(f"Your vehicle type is {vehicle_type}.")
@@ -65,36 +71,72 @@ if vehicle_type in ["BIKE", "CAR", "TRUCK"]:
     else:
         vehicle_charges = 50
 
+    if parking_on_weekends == "YES":
+        weekend_charges = 100
+    
+    elif parking_on_weekends == "NO":
+        weekend_charges = 0
+
+    else:
+        print("Invalid input. Choose YES or NO.")
+        weekend_charges = 0
+
     if len(vehicle_number) == 10 and vehicle_number.isalnum():
     
         if hours_parked <= 0:
             print(f"Invalid hours. \n")
 
         elif 1 <= hours_parked <= 2:
-            print("Thank you for entering your details here, please find your receipt: \n")
             add_20 = hours_parked * 20
-            print(f"You have parked your {vehicle_type} for {hours_parked} hour(s).")
-            print(f"The final amount to be paid is ₹{add_20 + vehicle_charges}.\n")
+            total_amount = add_20
+            receipt_amount = weekend_charges + total_amount + vehicle_charges
+
+            print(f"Parking charges: ₹{add_20}")
+            print(f"Vehicle charges: ₹{vehicle_charges}")
+            print(f"Weekend charges: ₹{weekend_charges}")
+            gst_18 = (receipt_amount * 18) / 100
+            print(f"GST (18%): ₹{gst_18:.2f}")
+            total_receipt_amount = receipt_amount + gst_18
+            print(f"Total amount: ₹{total_receipt_amount}\n")
 
         elif 3 <= hours_parked <= 5:
-            print("Thank you for entering your details here, please find your receipt: \n")
             add_15 = hours_parked * 15
-            print(f"You have parked your {vehicle_type} for {hours_parked} hour(s).")
-            print(f"The final amount to be paid is ₹{add_15 + vehicle_charges}.\n")
-
+            total_amount = add_15
+            receipt_amount = weekend_charges + total_amount + vehicle_charges
+            
+            print(f"Parking charges: ₹{add_15}")
+            print(f"Vehicle charges: ₹{vehicle_charges}")
+            print(f"Weekend charges: ₹{weekend_charges}")
+            gst_18 = (receipt_amount * 18) / 100
+            print(f"GST (18%): ₹{gst_18:.2f}")
+            total_receipt_amount = receipt_amount + gst_18
+            print(f"Total amount: ₹{total_receipt_amount}\n")
 
         elif 6 <= hours_parked <= 10:
-            print("Thank you for entering your details here, please find your receipt: \n")
             add_10 = hours_parked * 10
-            print(f"You have parked your {vehicle_type} for {hours_parked} hour(s).")
-            print(f"The final amount to be paid is ₹{add_10 + vehicle_charges}.\n")
+            total_amount = add_10
+            receipt_amount = weekend_charges + total_amount + vehicle_charges
+            
+            print(f"Parking charges: ₹{add_10}")
+            print(f"Vehicle charges: ₹{vehicle_charges}")
+            print(f"Weekend charges: ₹{weekend_charges}")
+            gst_18 = (receipt_amount * 18) / 100
+            print(f"GST (18%): ₹{gst_18:.2f}")
+            total_receipt_amount = receipt_amount + gst_18
+            print(f"Total amount: ₹{total_receipt_amount}\n")
 
         else:
             surcharge_fee = (hours_parked * 10) + 150
-            print("Thank you for entering your details here, please find your receipt: \n")
-            print(f"You have parked your {vehicle_type} for {hours_parked} hour(s). There is surcharge fee of ₹150 on your total amount.")
-            print(f"The amount to be paid is ₹{surcharge_fee}.")
-            print(f"The final amount to be paid is ₹{surcharge_fee + vehicle_charges}.\n")
+            total_amount = surcharge_fee
+            receipt_amount = weekend_charges + total_amount + vehicle_charges
+            
+            print(f"Parking charges: ₹{surcharge_fee}")
+            print(f"Vehicle charges: ₹{vehicle_charges}")
+            print(f"Weekend charges: ₹{weekend_charges}")
+            gst_18 = (receipt_amount * 18) / 100
+            print(f"GST (18%): ₹{gst_18:.2f}")
+            total_receipt_amount = receipt_amount + gst_18
+            print(f"Total amount: ₹{total_receipt_amount}\n")
 
     else:
         print("The vehicle number is invalid. \n")
