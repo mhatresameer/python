f7169a52f4bcd53c3b8b48adf08a5cf07b66ec17
M. Sameer <mhatresameer@outlook.com>
2026-06-21 11:51:17 +0530

    Create 5.0.1_loops_print_1_to_10.py

---
+++ 5.0.1_loops_print_1_to_10.py
@@ -0,0 +1,10 @@
+"""
+print numbers using initial and end number
+For example (1 to 10)
+"""
+count = int(input("Enter the number you wish to start from: "))
+end = int(input("Enter the number you wish to end at: "))
+
+while count <= end:
+  print(count)
+  count += 1
