ef63d5ace1c102189538e15a62041999745dfbd8
M. Sameer <mhatresameer@outlook.com>
2026-07-07 18:49:46 +0530

    Create 5.0.16_loops_print_numbers_from_N_to_1.py

---
+++ 5.0.16_loops_print_numbers_from_N_to_1.py
@@ -0,0 +1,13 @@
+# using WHILE loop
+n = int(input("Enter a number: "))
+
+while n >= 1:
+    print(n)
+    n -= 1
+
+
+# using FOR loop
+n = int(input("Enter a number: "))
+
+for i in range(n, 0, -1):
+    print(i)
