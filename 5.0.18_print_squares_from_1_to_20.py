ff96e7417c5e97fc3f9f4d9491c8b1b07b9e01f7
M. Sameer <mhatresameer@outlook.com>
2026-07-09 21:16:24 +0530

    Create 5.0.18_print_squares_from_1_to_20.py

---
+++ 5.0.18_print_squares_from_1_to_20.py
@@ -0,0 +1,15 @@
+# using FOR loop
+for i in range(1, 21):
+    print(i * i)
+
+# using WHILE loop
+i = 1
+
+while i <= 20:
+    print(i * i)
+    i += 1
+
+
+# using FOR loop in one line
+for i in range(1, 21):
+    print(i * i, end=" ")
