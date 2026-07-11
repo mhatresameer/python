f7a24ee101d1e688a96f97702217551c3c65e0f7
M. Sameer <mhatresameer@outlook.com>
2026-07-11 21:51:19 +0530

    Create 5.0.20_print_the_first_N_natural_numbers.py

---
+++ 5.0.20_print_the_first_N_natural_numbers.py
@@ -0,0 +1,15 @@
+# using WHILE loop
+n = int(input("Enter a number: "))
+
+i = 1
+
+while i <= n:
+    print(i)
+    i += 1
+
+
+# using FOR loop
+n = int(input("Enter a number: "))
+
+for i in range(1, n + 1):
+    print(i)
